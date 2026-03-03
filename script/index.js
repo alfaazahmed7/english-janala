292d850b02740f7698f63b5f68cefd51dfd4461a
Alfaaz Ahmed <alfaazahmed010@gmail.com>
2026-03-04 00:21:40 +0600

    added a section for empty lesson and fixed some errors for better ux

--- script/index.js
+++ script/index.js
@@ -18,13 +18,23 @@ const displayLevelWord = (words) => {
     const wordContainer = document.getElementById("word-container");
     wordContainer.innerHTML = "";
 
+    if (words.length == 0) {
+        wordContainer.innerHTML = `
+        <div class="text-center col-span-full rounded-xl py-10 space-y-3">
+                <img class = "mx-auto" src = "./assets/alert-error.png">
+                <p class="text-[13px] text-[#79716B] font-bangla">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                <h2 class="font-bangla text-[#292524] font-medium text-[34px]">নেক্সট Lesson এ যান</h2>
+            </div>
+        `;
+    }
+
     words.forEach((word) => {
         const card = document.createElement("div");
         card.innerHTML = `
         <div class="bg-white rounded-lg shadow-sm text-center py-10 px-5 space-y-4">
-                <h2 class="font-bold text-2xl">${word.word}</h2>
+                <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
                 <p class="font-semibold">Meaning/Pronuounciation</p>
-                <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+                <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "Pronounciation পাওয়া  যায়নি"}"</div>
                 <div class="flex justify-between items-center">
                     <button class="btn bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-circle-info"></i></button>
                     <button class="btn bg-[#1a91ff10] hover:bg-[#1a91ff80]"><i class="fa-solid fa-volume-high"></i></button>
